be7b7a4881d60312c97f7b850c20df4f261d913c
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-12 15:40:26 +0100

    Sviluppo: - Creato l'array contenente gli elementi da stampare nel menu di navigazione; - Inserito il logo; - Creata la barra di navigazione;

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,120 @@
 <script>
 export default {
     name: "AppHeader",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+            ],
+        }
+    },
 }
 </script>
 
 <template>
     <header>
+        <div class="container">
+            <!-- slot contenente il logo -->
+            <div class="slot_logo">
+                <a href="#">
+                    <img src="../assets/img/dc-logo.png" alt="logo_DC">
+                </a>
+            </div>
 
+            <!-- menu di navigazione -->
+            <div class="slot_nav_menu">
+                <ul>
+                    <li v-for="(link, index) in links" :key="index">
+                        <a :class="{ active: link.current }" :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
 
+header {
+    height: 100px;
+}
+
+.container {
+    height: 100%;
+    @include between();
+}
+
+.slot_logo {
+    width: 100px;
+    height: 80px;
+}
+
+.slot_nav_menu {
+    height: 100%;
+
+    ul {
+        @include center();
+        height: 100%;
+
+        li {
+            list-style-type: none;
+            margin-left: 20px;
+
+            a {
+                text-decoration: none;
+            }
+        }
+    }
+}
 </style>
