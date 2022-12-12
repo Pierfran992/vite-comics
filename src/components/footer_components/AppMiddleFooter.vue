2c2da744c9080b0cb4f0c404a561f7d30cf6c0c9
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-12 18:57:09 +0100

    Stato sviluppo AppMiddleFooter: - Creata la section principale con lo sfondo; - creato il div in cui inserire i vari menu; -creato il div in cui ho inserito il big logo DC;

--- src/components/footer_components/AppMiddleFooter.vue
+++ src/components/footer_components/AppMiddleFooter.vue
@@ -1,13 +1,39 @@
 <script>
 export default {
-
+    name: "AppMiddleFooter"
 }
 </script>
 
 <template>
+    <!-- creo la sezione principale che prenderà l'intera lunghezza della pagina -->
+    <section>
+        <div class="container">
+            <!-- slot che conterrà i vari menu del footer -->
+            <div class="container_menu">
 
+            </div>
+
+            <!-- slot che conterrà il logo del footer -->
+            <div class="slot_big_logo">
+                <img src="../../assets/img/dc-logo-bg.png" alt="big logo">
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+@use '../../styles/partials/mixins' as *;
 
+section {
+    background-image: url("../../assets/img/footer-bg.jpg");
+    background-size: cover;
+    height: 400px;
+    overflow: hidden;
+}
+
+.container {
+    height: 100%;
+    @include between();
+}
 </style>
