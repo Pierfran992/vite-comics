31443e14a753b4f255ac3c55bbcb10136ff91adc
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-12 19:38:28 +0100

    Stato Sviluppo AppMiddleFooter: -Creato l'array nel data contenente i vari menu e i relativi link;

--- src/components/footer_components/AppMiddleFooter.vue
+++ src/components/footer_components/AppMiddleFooter.vue
@@ -1,6 +1,132 @@
 <script>
 export default {
-    name: "AppMiddleFooter"
+    name: "AppMiddleFooter",
+    data() {
+        return {
+            menu: [
+                {
+                    title: "DC COMICS",
+                    link: [
+                        {
+                            url: "#",
+                            text: "Characters"
+                        },
+                        {
+                            url: "#",
+                            text: "Comics"
+                        },
+                        {
+                            url: "#",
+                            text: "Movies"
+                        },
+                        {
+                            url: "#",
+                            text: "TV"
+                        },
+                        {
+                            url: "#",
+                            text: "Games"
+                        },
+                        {
+                            url: "#",
+                            text: "Videos"
+                        },
+                        {
+                            url: "#",
+                            text: "News"
+                        },
+                    ]
+                },
+                {
+                    title: "SHOP",
+                    link: [
+                        {
+                            url: "#",
+                            text: "Shop DC"
+                        },
+                        {
+                            url: "#",
+                            text: "Shop DC Collectibles"
+                        },
+                    ]
+                },
+                {
+                    title: "DC",
+                    link: [
+                        {
+                            url: "#",
+                            text: "Terms Of Use"
+                        },
+                        {
+                            url: "#",
+                            text: "Privacy policy (New)"
+                        },
+                        {
+                            url: "#",
+                            text: "Ad Choices"
+                        },
+                        {
+                            url: "#",
+                            text: "Advertising"
+                        },
+                        {
+                            url: "#",
+                            text: "Jobs"
+                        },
+                        {
+                            url: "#",
+                            text: "Subscriptions"
+                        },
+                        {
+                            url: "#",
+                            text: "Talent Workshops"
+                        },
+                        {
+                            url: "#",
+                            text: "CPSC Certificates"
+                        },
+                        {
+                            url: "#",
+                            text: "Ratings"
+                        },
+                        {
+                            url: "#",
+                            text: "Shop Help"
+                        },
+                        {
+                            url: "#",
+                            text: "Contact Us"
+                        },
+                    ]
+                },
+                {
+                    title: "SITES",
+                    link: [
+                        {
+                            url: "#",
+                            text: "DC"
+                        },
+                        {
+                            url: "#",
+                            text: "MAD Magazine"
+                        },
+                        {
+                            url: "#",
+                            text: "DC kids"
+                        },
+                        {
+                            url: "#",
+                            text: "DC Universe"
+                        },
+                        {
+                            url: "#",
+                            text: "DC Power Visa"
+                        },
+                    ]
+                }
+            ]
+        }
+    }
 }
 </script>
 
