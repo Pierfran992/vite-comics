5bcbfb4795937ebf9245d5db4fa9e4b1b1790415
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-13 16:17:27 +0100

    Inseriti i componenti delle card nel componente AppMain;

---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+import ListCard from './main_components/ListCard.vue';
+export default {
+    name: "AppMain",
+    components: {
+        ListCard
+    }
+}
+</script>
+
+<template>
+    <section>
+        <!-- richiamo il componente che contiene le card dei fumetti -->
+        <ListCard />
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    background-color: #1c1c1c;
+    padding: 50px 0;
+}
+</style>
