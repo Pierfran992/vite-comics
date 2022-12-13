c3c789db23d2a39a47a927485b96065e5627278e
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-13 18:31:27 +0100

    Creato il badge che indica la serie di fumetti visualizzati in pagina;

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,14 +10,30 @@ export default {
 
 <template>
     <section>
-        <!-- richiamo il componente che contiene le card dei fumetti -->
-        <ListCard />
+        <div class="container">
+            <div class="badge">
+                <h2>CURRENT SERIES</h2>
+            </div>
+            <!-- richiamo il componente che contiene le card dei fumetti -->
+            <ListCard />
+        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
 section {
     background-color: #1c1c1c;
     padding: 50px 0;
+}
+
+.badge {
+    background-color: $secondaryColor;
+    width: fit-content;
+    font-size: 20px;
+    padding: 10px 30px;
+    position: relative;
+    top: -80px;
 }
 </style>
